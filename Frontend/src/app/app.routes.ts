f3c52f425c8214c7bff8b91007a57ca659e91ae8
GaboTex2004 <gabrielchoque222@gmail.com>
2026-04-26 10:40:32 -0400

    Limpieza manual de conflictos en schemas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,29 +5,15 @@ import { HomeComponent } from './features/home/home.component';
 import { LandingComponent } from './features/home-page/landing.component';
 import { ChatComponent } from './features/chat/chat.component';
 import { EmergenciasTallerComponent } from './features/emergencia/emergencia.component';
-<<<<<<< HEAD
-<<<<<<< ours
-=======
 import { DashboardComponent } from './features/home/dashboard/dashboard.component';
 import { ReportesComponent } from './features/reportes/reportes.component';
 import { ReportesAdminComponent }from './features/reportes-admin/reportes-admin.component'
->>>>>>> theirs
-=======
-import { DashboardComponent } from './features/home/dashboard/dashboard.component';
->>>>>>> origin/main
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-<<<<<<< HEAD
-<<<<<<< ours
-  { path: 'home', component: HomeComponent },
-  { path: 'emergencia', component: EmergenciasTallerComponent }, // Asegúrate de tener un HomeComponent creado
-=======
-=======
->>>>>>> origin/main
   { 
     path: 'home', 
     component: HomeComponent,
@@ -35,14 +21,8 @@ export const routes: Routes = [
       { path: '', component: DashboardComponent },
       { path: 'emergencia', component: EmergenciasTallerComponent },
       { path: 'chat', component: ChatComponent },
-<<<<<<< HEAD
       { path: 'reportes', component:ReportesComponent},
       { path: 'reportes-admin', component:ReportesAdminComponent},
     ]
   },
->>>>>>> theirs
-=======
-    ]
-  },
->>>>>>> origin/main
 ];
